50682fd886016612da863d9b1e249469c2ecf61c
Tran Do Hoa <trandohoa93@gmail.com>
2022-12-24 18:44:38 +0700

    Router/Layout

---
+++ src/routes/index.js
@@ -0,0 +1,2 @@
+export { default as privateRoute } from './privateRoute';
+export { default as protectedRoute } from './protectedRoute';
---
+++ src/routes/protectedRoute.js
@@ -0,0 +1,15 @@
+// Layouts
+import { HeaderOnly } from '~/layouts';
+
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+const protectedRoute = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+
+export default protectedRoute;
